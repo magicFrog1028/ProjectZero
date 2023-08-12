86dfca63870b6104059e9d8fc412677323e823e1
unknown <871556561@qq.com>
2023-08-12 13:53:16 +0800

    first service added, without db connection

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -1,9 +1,18 @@
+const userService = require("../services/userService.js");
+let userServices = new userService();
 const userRouter = async (server) =>{
 
 
     server.post(
-        '/getUser', async function handler (request, reply) {
-            return "Hello its Kris"
+        '/getUsername', async function handler (request, reply) {
+            return userServices.getUsername();
+        }
+    );
+
+
+    server.post(
+        '/getPassword', async function handler (request, reply) {
+            return userServices.getPassword();
         }
     );
     
---
+++ server/services/userService.js
@@ -0,0 +1,13 @@
+
+module.exports = class userService{
+
+    constructor(){};
+
+    getUsername(){
+        return "Admin";
+    }
+
+    getPassword(){
+        return "Admin";
+    }
+}
