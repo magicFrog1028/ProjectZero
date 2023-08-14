915e67d7e2eb7fad546db4a60c5045b142d6c121
kris <1373130621@qq.com>
2023-08-14 11:11:40 +0800

    add loginView

--- ui/.eslintrc.cjs
+++ ui/.eslintrc.cjs
@@ -3,12 +3,25 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    'prettier/prettier': [
+      'warn',
+      {
+        semi: false,
+        tabWidth: 2,
+        singleQuote: true,
+        printWidth: 80,
+        trailingComma: 'none',
+        endOfLine: 'auto'
+      }
+    ]
   }
 }
--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -1,8 +1,32 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import LoginView from '@/views/auth/LoginView.vue'
+const routes = [
+  { path: '/' },
+  {
+    path: '/auth',
+    redirect: '/auth/login',
+    children: [
+      { path: '/auth/login', component: LoginView },
+      { path: '/auth/register' }
+    ]
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes
 })
-
+// router.beforeEach((to, from) => {
+//   // 而不是去检查每条路由记录
+//   // to.matched.some(record => record.meta.requiresAuth)
+//   if (to.meta.requiresAuth && !auth.isLoggedIn()) {
+//     // 此路由需要授权，请检查是否已登录
+//     // 如果没有，则重定向到登录页面
+//     return {
+//       path: '/login',
+//       // 保存我们所在的位置，以便以后再来
+//       query: { redirect: to.fullPath }
+//     }
+//   }
+// })
 export default router
